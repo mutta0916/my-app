ede4b487df2086030d265ffc0c7f830c5b61e753
mutta0916 <mutta0916@gmail.com>
2022-04-16 08:45:55 +0900

    一時コミット

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return (
+    <>
+      <h1>Hello world!あ</h1>
+      <p>お元気ですか？</p>
+      <table border="1">
+        <tr>
+          <th>名前</th>
+          <th>レベル</th>
+          <th>属性</th>
+        </tr>
+        <tr>
+          <td>ゆった</td>
+          <td>1</td>
+          <td>無</td>
+        </tr>
+        <tr>
+          <td>ぱい</td>
+          <td style={{ color: `red` }}>0</td>
+          <td>のーまる</td>
+        </tr>
+      </table>
+    </>
+    );
+  ;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById("root"));
